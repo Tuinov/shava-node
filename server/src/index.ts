412e10134498a1372f239092d844784e4adb7a6e
Tuinov <tuinovjob@gmail.com>
2022-02-25 22:49:19 +0300

    finish

---
+++ server/src/index.ts
@@ -0,0 +1,36 @@
+import express, { Request, Response } from 'express';
+// import cookieSession from 'cookie-session';
+import expressSession from 'express-session'
+import { AppRouter } from './AppRouter';
+import './controllers/LoginController';
+import './controllers/ProductController';
+import './controllers/OrderController';
+import bodyParser, { json } from 'body-parser';
+import 'dotenv/config'
+import cors from 'cors';
+
+const app = express();
+app.use(cors());
+
+app.use(json());
+app.use(express.urlencoded({ extended: true }));
+// app.use(cookieSession({ keys: ['asfadsfadfs'] }));
+
+// app.use(express.static(path.resolve(__dirname, '../client/build')));
+
+app.use(
+  expressSession({
+    secret: 'sjsjkfrjkfkrjf',
+    resave: true,
+    saveUninitialized: false,
+  })
+);
+
+app.use(AppRouter.getInstance());
+
+let PORT = process.env.PORT || 5000;
+
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
